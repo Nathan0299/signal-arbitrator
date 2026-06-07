a5db93cc06a44589b1e1d75893046294f2231ce7
Nathan's <macpro@Nathans-MacBook-Pro.local>
2026-06-07 13:29:29 +0000

    style: untrack node_modules, build output, sales assets, and tarball

--- dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './SignalArbitrator';
-export * from './PriorityEvaluator';
-export * from './ConflictDetector';
-export * from './AggregationExecutor';
-export * from './EntityStateStore';
